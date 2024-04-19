78d4ed5138acb9f0776f854495a035c7d35da196
Masha <masha@MacBook-Air.local>
2024-04-19 22:21:58 +0200

    createCounter

--- main.js
+++ main.js
@@ -10,22 +10,34 @@ function calculateDiscountedPrice (products, discount) {
     name: item.name,
     price: item.price * (100 - discount) / 100,
   }));
+
   return productsWithDiscountedPrices;
 }
 
 function getFullName (persons) {
   const firtsName = (persons.firtsName)[0].toUpperCase() + (persons.firtsName).slice(1);
   const lastName = (persons.lastName)[0].toUpperCase() + (persons.lastName).slice(1);
+  
   return (`${firtsName} ${lastName}`);
 }
 
 function filterUniqueWords(text) {
   const words = text.toLowerCase().match(/\b[\w'-]+\b/g);
   const uniqueWords = words.filter((item, index) => words.indexOf(item) === index).sort();
+  
   return uniqueWords;
 }
 
 function getAverageGrade (students) {
   const allStudentsGrades = students.flatMap(student => student.grades);
+  
   return allStudentsGrades.reduce((totalGrade, grade) => totalGrade + grade, 0) / allStudentsGrades.length; 
+}
+
+function  createCounter () {
+  let currentCount = 1;
+
+  return function() { 
+    return currentCount++;
+  };
 }
